aa4d38ab88f92f4adca3bcb073355a8cba9d9526
Sarthak Pandey <sarthakpandey1998@gmail.com>
2023-12-07 20:12:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TextFormatter from "./TextFormatter";
+
+function App() {
+  return (
+    <div className="App">
+      <TextFormatter />
+    </div>
+  );
+}
+
+export default App;
